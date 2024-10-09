8af44687f7e0c3590569cfb58ac92e3d0e875b7f
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-10-09 13:54:03 +0200

    urls

--- prototype1/prototype1/urls.py
+++ prototype1/prototype1/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
